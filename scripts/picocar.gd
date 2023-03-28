893addca802a19345fc520080a1b8ffffb7a8f7a
Tom Murray <tom.paul.murray@gmail.com>
2023-03-28 16:49:04 +0100

    Start to add some rotation of the wheels in response to input events
    Need to work out the physics next.

--- scripts/picocar.gd
+++ scripts/picocar.gd
@@ -15,38 +15,48 @@ extends CharacterBody3D
 # Array of wheel nodes accessible by flattened 2-d index
 # [y, x] e.g. front-right = (1, 1) flattened index = 3
 @onready var wheels = [$wheel_bl, $wheel_br, $wheel_fl, $wheel_fr]
+enum Wheel {
+	BACK_L,
+	BACK_R,
+	FRONT_L,
+	FRONT_R
+}
+
+const MAX_STEER : float = deg_to_rad(40.0)
+const STEER_SPEED : float = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _physics_process(delta):
-	# Here is where we deal with input and manage driving the car
-	# when using CharacterBody3D to control the car.
+	# We need to consider the forces on each wheel (acceleration on front
+	# wheels, in the direction they're going, friction preventing tyres
+	# from sliding against the ground) and integrate these to give the motion
+	# of the car as a whole.
 	
-	# We are going to implement our car's motion 2-dimensionally for
-	# the learning experience. 2-dimensionally meaning x/z axes as this
-	# is a top-down game.
+	# Apply correct visual rotation to the front wheels
+	if not Engine.is_editor_hint():
+		var steer = Input.get_axis("steer_left", "steer_right");
+		var accel = Input.get_axis("brake_reverse", "accelerate");
+		
+		# Take front wheels
+		var front_wheels = wheels.slice(2, 4)
+		
+		for wheel in front_wheels:
+			# TODO: Fix car scene so that the car is facing forwards
+			# rather than backwards...
+			# Take the difference, rotate wheel smoothly
+			var target_rotation_y = -steer * MAX_STEER
+			var diff = target_rotation_y - wheel.rotation.y
+			var rotate_amount = diff * delta * STEER_SPEED
+			if abs(diff) < 0.1:
+				rotate_amount = diff
+			wheel.rotation.y += rotate_amount
 	
-	# We want the motion of the car to mirror that of a real car. I will
-	# not (at least initially) implement anything like drift etc. So the
-	# motion wanted is where either the front wheels pull or the back
-	# wheels push, and the car's front moves in the direction the front
-	# wheels are pointed, and the car's back slowly converges to that
-	# direction also.
-	
-	# I guess we can think of the maths for this initially like a series
-	# of discrete steps where we move the front of the car in the direction
-	# of the front wheels, and then move the back of the car to line up
-	# with this and maintain the length of the car. Considering this in
-	# ever finer time steps should lead us to some formula for x/y.
-	# Even now I'm aware this probably does not give the motion we want
-	# because the back wheels will spin more or less depending on the
-	# direction of steering. The forces on each wheel need to be
-	# reconciled all together to produce the final rotation/position of
-	# the car each physics step.
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
+		
 	pass
