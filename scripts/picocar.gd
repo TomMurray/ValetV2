ccb5a68ec6e40e85493c305271c4b9f301713e95
Tom Murray <tom.paul.murray@gmail.com>
2023-03-30 16:19:00 +0100

    A semi-working implementation of car movement
    Main missing component is carrying the velocity of the car forward between physics steps.

--- scripts/picocar.gd
+++ scripts/picocar.gd
@@ -14,7 +14,13 @@ extends CharacterBody3D
 
 # Array of wheel nodes accessible by flattened 2-d index
 # [y, x] e.g. front-right = (1, 1) flattened index = 3
-@onready var wheels = [$wheel_bl, $wheel_br, $wheel_fl, $wheel_fr]
+@onready var wheels : Array[Node3D] = [
+	$wheel_bl as Node3D,
+	$wheel_br as Node3D,
+	$wheel_fl as Node3D,
+	$wheel_fr as Node3D
+]
+
 enum Wheel {
 	BACK_L,
 	BACK_R,
@@ -23,11 +29,16 @@ enum Wheel {
 }
 
 const MAX_STEER : float = deg_to_rad(40.0)
-const STEER_DEG_PER_SEC : float = deg_to_rad(120.0)
+const STEER_PER_SEC : float = deg_to_rad(120.0)
+
+const ACCEL_PER_SEC : float = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+
+func get_node3d_midpoint(a : Array[Node3D]):
+	return a.map(func(x : Node3D): return x.transform.origin).reduce(func(a, b): return a + b) / a.size()
 
 func _physics_process(delta):
 	# We need to consider the forces on each wheel (acceleration on front
@@ -46,9 +57,86 @@ func _physics_process(delta):
 		
 		# Left wheel of each set has canonical rotation
 		var curr_steer_angle = front_wheels[0].rotation.y
-		curr_steer_angle = clamp(curr_steer_angle - steer * STEER_DEG_PER_SEC * delta, -MAX_STEER, MAX_STEER)
+		curr_steer_angle = clamp(curr_steer_angle - steer * STEER_PER_SEC * delta, -MAX_STEER, MAX_STEER)
+		
+		# Set visual rotation of front wheels
 		for wheel in front_wheels:
 			wheel.rotation.y = curr_steer_angle
+			
+		# Calculate updated rotation of car as a whole. This is based
+		# on a simple 2-wheel "bicycle" physics model as elaborated nicely
+		# here: http://engineeringdotnet.blogspot.com/2010/04/simple-2d-car-physics-in-games.html
+		# The 4-wheel model involves integrating forces on all 4 wheels but
+		# I don't believe it adds much to the feeling of steering the car.
+		
+		# The method is to find the midpoint of the front and back wheels,
+		# move them both by the desired velocity, then rotate and move the
+		# car to match the 2 new positions. The distance does not remain
+		# equal between front and back in this process - you can prove this
+		# fairly easily by imagining 2 perpendicular lines of length l:
+		#
+		#  ^
+		#  |
+		#  |
+		#  |
+		#  a<---b
+		# 
+		# Here ||b - a|| = l
+		# Moving halfway along each of these lines gives:
+		#
+		#  ^
+		#  |
+		#  a'
+		#  |
+		#  <-b'-
+		#
+		# Here ||b' - a'|| = sqrt(2 * (l/2)^2) != l
+		#
+		# However this doesn't really matter as we only use
+		# this to give a new position and rotation for the next frame
+		# and then start again.
+		var back_midpoint = get_node3d_midpoint(back_wheels)
+		var front_midpoint = get_node3d_midpoint(front_wheels) as Vector3
+		back_midpoint = Vector2(back_midpoint.x, back_midpoint.z)
+		front_midpoint = Vector2(front_midpoint.x, front_midpoint.z)
+		var old_pos_rel = (front_midpoint + back_midpoint)/2
+		
+		# Move the front/back wheels of the "bicycle" by the desired
+		# amount in the direction they are facing. Note this is done
+		# relative to the transform of the car node itself.
+		var v = accel * ACCEL_PER_SEC * delta
+		back_midpoint.y += v
+		# Must be a more succinct way of doing this, but using sin/cos
+		# for now
+		front_midpoint.x += sin(curr_steer_angle) * v
+		front_midpoint.y += cos(curr_steer_angle) * v
+		
+		# Calculate new body rotation delta - this assumes the original
+		# vector between back and front bicycle wheel was y axis vector.
+		# We treat the new line between back and front bicycle wheel and
+		# y axis as a right-angled triangle and calculate the delta in
+		# rotation from this (using inverse tangent)
+		var new_bike_dir = front_midpoint - back_midpoint
+		var new_body_rot = atan(new_bike_dir.x/new_bike_dir.y)
+		rotate(Vector3.UP, new_body_rot)
+		
+		# Because our bike wheel's positions are relative to car body
+		# node here, our velocity for this physics update is just the
+		# delta between the old midpoint of the bicycle wheels and the
+		# new one. 
+		var new_pos_rel = (front_midpoint + back_midpoint) / 2
+		var pos_delta = new_pos_rel - old_pos_rel
+		
+		# Velocity is independent of delta, but our calculated delta is
+		# already multiplied by our delta, so undo this.
+		# We multiply the movement delta by the basis rotation to move in
+		# the direction the car is facing.
+		#
+		# Note use of get_rotation_quaternion because basis of the car also
+		# encodes scale, and we want to apply rotation independent of scale.
+		velocity = basis.get_rotation_quaternion() * Vector3(pos_delta.x, 0, pos_delta.y) * (1/delta)
+		
+		move_and_slide()
 	
 	pass
 
