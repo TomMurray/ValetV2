cb40709a10092ffcba999c32b7e58246b1e89d4d
Tom Murray <TomMurray@users.noreply.github.com>
2024-01-10 16:03:49 +0000

    Avoid constantly changing the material ID by checking if we have a surface material override already or not

--- scripts/road_paint_cuboid.gd
+++ scripts/road_paint_cuboid.gd
@@ -3,8 +3,9 @@ extends MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Duplicate the material on load - we want to uniquely pass on the uniforms
-	set_surface_override_material(0, mesh.surface_get_material(0).duplicate())
+	if get_surface_override_material_count() == 0:
+		# Duplicate the material on load - we want to uniquely pass on the uniforms
+		set_surface_override_material(0, mesh.surface_get_material(0).duplicate())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
