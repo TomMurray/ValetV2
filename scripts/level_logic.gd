c7983780a60606f606ca0e0bf57904b6fc7dd4a6
Tom Murray <TomMurray@users.noreply.github.com>
2024-01-11 13:00:56 +0000

    Move UI theme to resources (instead of overrides), update UI to show current obstacles hit

--- scripts/level_logic.gd
+++ scripts/level_logic.gd
@@ -1,16 +1,15 @@
 extends Node
 class_name LevelLogic
 
-signal objectives_progress_changed
-signal complete(success : bool)
-
-var was_completed : bool = false
-var outcome : bool = false
-
 # Class responsible for tracking level objectives, and sending out signals for
 # updated objectives (obstacles hit/max, level complete/failed etc.)
 
+signal objectives_progress_changed
+signal complete(success : bool)
+
 @export var objective : LevelObjective
+var was_completed : bool = false
+var outcome : bool = false
 var obstacles_hit : int = 0
 
 func get_obstacles_hit() -> int:
@@ -19,10 +18,16 @@ func get_obstacles_hit() -> int:
 func get_max_obstacles_hit() -> int:
 	return objective.max_obstacles_hit
 
+func is_complete() -> bool:
+	return was_completed
+	
+func get_outcome() -> bool:
+	return outcome
+
 func on_obstacle_hit(obstacle : ObstacleComponent):
 	obstacles_hit += 1
 	objectives_progress_changed.emit()
-	if obstacles_hit > get_max_obstacles_hit():
+	if obstacles_hit >= get_max_obstacles_hit():
 		on_complete(false)
 
 func on_complete(success : bool):
---
+++ scripts/obstacles_counter.gd
@@ -0,0 +1,26 @@
+@tool
+extends RichTextLabel
+class_name ObstaclesCounter
+
+@export var level_logic : LevelLogic
+
+func update_text():
+	if level_logic:
+		var limit : int = level_logic.get_max_obstacles_hit()
+		var curr : int = min(level_logic.get_obstacles_hit(), limit)
+		text = "%d/%d" % [curr, limit]
+		# Make the text increasingly red as it approaches the limit
+		var ratio : float = float(curr) / float(limit)
+		add_theme_color_override("default_color", lerp(Color.WHITE, Color.RED, ratio))
+		
+
+func _ready():
+	update_text()
+
+func _on_level_logic_objectives_progress_changed():
+	update_text()
+
+func _get_configuration_warnings():
+	if not level_logic:
+		return ["No LevelLogic attached to ObstaclesCounter"]
+	return []
