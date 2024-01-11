bccb832bfe322ae10811112b2fff497f52cecf41
Tom Murray <TomMurray@users.noreply.github.com>
2024-01-11 12:16:24 +0000

    Get basic success/failure state working

--- scripts/level_logic.gd
+++ scripts/level_logic.gd
@@ -2,7 +2,10 @@ extends Node
 class_name LevelLogic
 
 signal objectives_progress_changed
-signal objectives_failed
+signal complete(success : bool)
+
+var was_completed : bool = false
+var outcome : bool = false
 
 # Class responsible for tracking level objectives, and sending out signals for
 # updated objectives (obstacles hit/max, level complete/failed etc.)
@@ -20,4 +23,13 @@ func on_obstacle_hit(obstacle : ObstacleComponent):
 	obstacles_hit += 1
 	objectives_progress_changed.emit()
 	if obstacles_hit > get_max_obstacles_hit():
-		objectives_failed.emit()
+		on_complete(false)
+
+func on_complete(success : bool):
+	if not was_completed:
+		was_completed = true
+		outcome = success
+		complete.emit(outcome)
+
+func on_parked():
+	on_complete(true)
--- scripts/obstacle_component.gd
+++ scripts/obstacle_component.gd
@@ -4,6 +4,8 @@ class_name ObstacleComponent
 
 signal was_hit(hit : Hit)
 
+var was_hit_once := false
+
 var level_logic : LevelLogic = null:
 	get: return level_logic
 	set(value): level_logic = value; update_configuration_warnings()
@@ -20,5 +22,7 @@ func _get_configuration_warnings() -> PackedStringArray:
 # For now there is no feedback to the hitter (no return from this
 # function).
 func hit(hit : Hit) -> void:
-	level_logic.on_obstacle_hit(self)
+	if not was_hit_once:
+		was_hit_once = true
+		level_logic.on_obstacle_hit(self)
 	was_hit.emit(hit)
