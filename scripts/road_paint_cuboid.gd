95971cb8d7bd4fb4ce5843da3e10551db3bdeda1
Tom Murray <TomMurray@users.noreply.github.com>
2024-01-12 17:45:23 +0000

    Use the newly discovered instance uniform modifier for road paint shader - solves all my problems there

--- scripts/road_paint_cuboid.gd
+++ scripts/road_paint_cuboid.gd
@@ -1,15 +1,8 @@
 @tool
 extends MeshInstance3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	if get_surface_override_material_count() == 0:
-		# Duplicate the material on load - we want to uniquely pass on the uniforms
-		set_surface_override_material(0, mesh.surface_get_material(0).duplicate())
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Sync shader parameters with the node
-	var material = get_surface_override_material(0) as ShaderMaterial
+	var material = mesh.surface_get_material(0) as ShaderMaterial
 	if material:
 		material.set_shader_parameter("transform", global_transform)
