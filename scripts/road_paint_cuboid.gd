725acb9106976c068235192e13e13d9b56910520
Tom Murray <TomMurray@users.noreply.github.com>
2024-01-15 11:37:14 +0000

    Fix road paint shader properly - use MODEL_MATRIX

--- scripts/road_paint_cuboid.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends MeshInstance3D
-
-func _process(delta):
-	# Sync shader parameters with the node
-	var material = mesh.surface_get_material(0) as ShaderMaterial
-	if material:
-		material.set_shader_parameter("transform", global_transform)
