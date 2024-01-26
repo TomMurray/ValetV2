7885b3bf7cf6c355556b839fe97d190996547fa3
Tom Murray <TomMurray@users.noreply.github.com>
2024-01-26 16:37:45 +0000

    Add some smoke to a car that has been crashed into

--- scripts/car_obstacle.gd
+++ scripts/car_obstacle.gd
@@ -3,10 +3,15 @@ extends StaticBody3D
 class_name CarObstacle
 
 @onready var obstacle_component : ObstacleComponent = %ObstacleComponent
+
+@export var explosion_scene : PackedScene
+
 @export var level_logic : LevelLogic = null:
 	set(value):
 		level_logic = value
 		update_configuration_warnings()
+
+var was_hit_once := false
 
 func _get_configuration_warnings() -> PackedStringArray:
 	if not level_logic:
@@ -17,5 +22,9 @@ func _ready():
 	obstacle_component.level_logic = level_logic
 
 func _on_obstacle_component_was_hit(hit):
-	# For now, just remove the car model
-	queue_free()
+	if not was_hit_once:
+		# Spawn explosion scene where the obstacle is
+		var explosion = explosion_scene.instantiate() as Node3D
+		add_child(explosion)
+	was_hit_once = true
+	
